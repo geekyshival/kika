e2d07a04f2ca49dc8d9ce4092ffb75281d0f1805
JGD <shival.chadda@gmail.com>
2019-02-13 22:48:48 +0530

    hi

---
+++ app/src/main/java/com/ctdroid/testnew/CtdroidActivity.java
@@ -0,0 +1,13 @@
+package com.ctdroid.testnew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CtdroidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ctdroid);
+    }
+}
